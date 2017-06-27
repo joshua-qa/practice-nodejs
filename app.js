44e0faf5114111c74a0b546da4be7ba64b49b88c
joshua <jgchoi.qa@gmail.com>
2017-06-28 00:40:31 +0900

    app modify (dynamic route)

--- app.js
+++ app.js
@@ -4,8 +4,31 @@ app.use(express.static('public'));
 app.get('/', function(req, res) {
     res.send('Hello page');
 });
-app.get('/login', function(req, res) {
-    res.send('Login please');
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    for(var i = 0; i < 10; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+        <body>
+            Hello, Dynamic!
+            <ul>
+            ${lis}
+            </ul>
+            ${time}
+        </body>
+    </html>`;
+    res.send(output);
+});
+app.get('/route', function(req, res) {
+    res.send('Hello Router, <img src="/ktt.jpg">');
 });
 app.get('/sagiri', function(req, res) {
     res.send('<h2>Kawaii</h2>');
